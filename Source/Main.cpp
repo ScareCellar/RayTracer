86914e2ebde41a63009537c7e5c41293cdf2c122
ScareCellar <98201877+ScareCellar@users.noreply.github.com>
2025-12-03 21:37:50 -0700

    added camera to main and scene files

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -2,7 +2,7 @@
 #include "Framebuffer.h"
 
 #include <iostream>
-
+#include "Camera.h"
 int main() {
 	constexpr int SCREEN_WIDTH = 800;
 	constexpr int SCREEN_HEIGHT = 600;
@@ -13,7 +13,11 @@ int main() {
 	renderer.CreateWindow("Ray Tracer", SCREEN_WIDTH, SCREEN_HEIGHT);
 
 	Framebuffer framebuffer(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+	
 
+	float aspectRatio = framebuffer.width / framebuffer.height;
+	Camera camera(70.0f, aspectRatio);
+	camera.SetView({ 0, 0, 5 }, { 0, 0, 0 });
 	SDL_Event event;
 	bool quit = false;
 	while (!quit) {
